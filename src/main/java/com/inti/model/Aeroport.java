4377906fdc27addee10bdb5042d323ad0f7ddc87
theolorenzi <theo.lorenzi@laposte.net>
2022-10-28 12:46:37 +0200

    ajout d'une fonction et d'une variable

--- src/main/java/com/inti/model/Aeroport.java
+++ src/main/java/com/inti/model/Aeroport.java
@@ -7,49 +7,62 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 @Entity
 @Table
 public class Aeroport {
-	
+
 	@Id
 	private int id;
 	private String nom;
-	
+	@Transient
+	private int nbPistes;
+
 	@OneToMany(mappedBy = "aeroport", cascade = CascadeType.ALL)
 	private List<InfosEscale> listeIE;
-	
+
 	public Aeroport() {
 		super();
 	}
+
 	public Aeroport(int id, String nom) {
 		super();
 		this.id = id;
 		this.nom = nom;
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getNom() {
 		return nom;
 	}
+
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
+
 	public List<InfosEscale> getListeIE() {
 		return listeIE;
 	}
+
 	public void setListeIE(List<InfosEscale> listeIE) {
 		this.listeIE = listeIE;
 	}
+
 	@Override
 	public String toString() {
 		return "Aeroport [id=" + id + ", nom=" + nom + "]";
 	}
-	
-	
+
+	public void test() {
+		System.out.println("coucou");
+	}
 
 }
